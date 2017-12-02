28d8c6b26b84b7328a3f50f65ce021f22d510571
Dragon <32561033+fluffypuppykitty@users.noreply.github.com>
2017-12-02 09:17:49 -0800

    Vidhi's Update
    Added onto bad programs to find. Scripts can now set password policies and audit policies.

--- PSScr1.ps1
+++ PSScr1.ps1
@@ -17,10 +17,7 @@ $tools =@("Cain","nmap","keylogger","Armitage","Wireshark","Metasploit","netcat"
 "Alchemy Remote","Remote Exec","Execute This!","OsMonitor","Wiretap Pro","Spylab WebSpy","Active@Password Chang","Stellar Phoenix Password Recovery","Passware Password Recovery Kit","Password Unlocker Bundle","Offline NT Password & Registry Editor","Offline NT Password & Registry Editor",
 "ElcomSoft System Recovery","Trinity Rescue Kit","Teamviewer","PopPcap")
 
-
-
-
-
+}
 Write-host "Checking $extensionsPassware Password Recovery Kit
 foreach($ext in $extensions){
 	Write-host "Checking for .$ext files"
@@ -35,5 +32,30 @@ foreach($tool in $tools){
 	C:\Windows\System32\cmd.exe /C dir C:\*$tool* /s /b | Out-File "$path\checkFilesOutput\$tool.txt"
 }
 Write-host "Finished searching for tools"
-# Audit policeis
+#Simple script for setting password policies in Windows.
+@echo off
+echo Setting up password policies
+net accounts /minpwlen:7 /maxpwage:30 /minpwage:10 /uniquepw:1 /forcelogoff:no /lockoutduration:30 /lockoutthreshold:7 
+echo Password Policies Set:
+echo Length: 7
+echo Max Age: 30 Days
+echo Min Age: 10 Days
+echo Times Password can be resused: 1
+echo Force Logoff: no
+echo Lockout Duration: 30 Minutes
+echo Lockout Threshold: 7
+secedit.exe /export /cfg C:\secconfig.cfg
+echo Use this command to update edited file: secedit.exe /configure /db %windir%\securitynew.sdb /cfg C:\secconfig.cfg /areas SECURITYPOLICY
+echo Setting up auditing
+auditpol /set /category:"Audit System Events"/Failure:Success
+auditpol /set /category:"Audit Process Tracking"/Faliure:Success
+auditpol /set /category:"Audit Account Logon Events"/Faliure:Success
+auditpol /set /category:"Audit Account Management"/Faliure:Success
+auditpol /set /category:"Audit Directory Service Access"/Faliure:Success
+auditpol /set /category:"Audit Logon Events"/Faliure:Success
+auditpol /set /category:"Audit Object Access"/Faliure:Success
+auditpol /set /category:"Audit Policy Change"/Faliure:Successs
+auditpol /set /category:"Audit Privilege Use"/Faliure:Success
+auditpol /set /category:"Audit Process Tracking"/Faliure:Success
+auditpol /set /cateory:"Audit System Events"/Faliure:Success
 
